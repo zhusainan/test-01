f8e75b2c96448f8500557497c771dd878d17c472
zhusainan <zhusnan@126.com>
2018-05-30 22:29:30 +0800

    update

---
+++ src/main/java/testa/TestA.java
@@ -0,0 +1,10 @@
+package main.java.testa;
+
+/**
+ * Created by SainanZhu on 5/26/18.
+ */
+public class TestA {
+    public static void main(String args[]){
+        System.out.println("testa");
+    }
+}
